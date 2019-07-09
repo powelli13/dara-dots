33c3b1ebf670573753c6294093a958ee0c60a0d9
Isaac Powell <powelli91@outlook.com>
2019-07-09 07:31:06 -0700

    added board creation. need to figure out best practice for alpha

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 700,
     physics: {
         default: 'arcade',
         arcade: {
@@ -16,12 +16,17 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var boardDotGroup;
+// var boardXSpacing = 100;
+var boardSpacing = 100;
+
 var dotGroup;
 
 function preload ()
 {
     this.load.image('background', 'assets/images/background.jpg');
     this.load.image('dot', 'assets/images/dot.png');
+    this.load.image('boardDot', 'assets/images/board_dot.png');
 }
 
 function create ()
@@ -32,6 +37,19 @@ function create ()
     dotGroup = this.add.group();
     dotGroup.add(dotImage);
 
+    // Setup the board
+    boardDotGroup = this.add.group();
+
+    for (let x = 0; x < 5; x++)
+    {
+        for (let y = 0; y < 5; y++)
+        {
+            let boardDotImage = this.add.image((x+1) * boardSpacing, (y+1) * boardSpacing, 'boardDot');
+            boardDotImage.setAlpha(1);
+            boardDotGroup.add(boardDotImage);
+        }
+    }
+     
     this.input.mouse.disableContextMenu();
 
     this.input.on('pointerup', function(p){
