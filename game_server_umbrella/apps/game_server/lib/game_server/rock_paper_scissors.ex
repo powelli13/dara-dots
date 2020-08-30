df150f8736e245e58ba0d5067122e5499a73f35e
Isaac Powell <powelli91@outlook.com>
2020-08-30 12:23:04 -0700

    continued basic RPS game implementation.

--- game_server_umbrella/apps/game_server/lib/game_server/rock_paper_scissors.ex
+++ game_server_umbrella/apps/game_server/lib/game_server/rock_paper_scissors.ex
@@ -6,6 +6,12 @@ defmodule GameServer.RockPaperScissors do
   """
   use GenServer
 
+  @defeats %{
+    :rock => :scissors,
+    :scissors => :paper,
+    :paper => :rock
+  }
+
   # Client methods
   # TODO add unique player ids assigned by the server
   @doc """
@@ -17,10 +23,10 @@ defmodule GameServer.RockPaperScissors do
     GenServer.call(game_name, {:player_move, player_name, move})
   end
 
-  def start_link(name) do
+  def start_link(name, init_arg) do
     GenServer.start_link(
       __MODULE__,
-      name,
+      init_arg,
       name: via_tuple(name))# TODO via_tuple here
   end
 
@@ -29,17 +35,51 @@ defmodule GameServer.RockPaperScissors do
   end
 
   @impl true
-  def init(opts) do
+  def init([first_player: first_player, second_player: second_player]) do
     # data structure to record player names and move inputs
     # when both players move calculate win/loss/draw and
     # send to scoreboard
     # TODO pull player names from opts for state initialization
-    {:ok, %{}}
+    {:ok, %{first_player => nil, second_player => nil}}
   end
 
   @impl true
-  def handle_call({:player_move, player_name, move}, _from, move_state) do
-    # TODO implement
-    {:reply, "#{player_name} gave a move of #{move}", move_state}
+  def handle_call({:player_move, player_name, move}, _from, game_state) do
+    # TODO validate attempted player name and move
+    # should probably handle that using a separate game state struct
+    game_state = Map.put(game_state, player_name, move)
+
+    # TODO should store more info int he game state struct
+    # have some way to kill this process after it reports the
+    # game being over.
+    # Check for any winner
+    case check_victory(game_state) do
+      # TODO report win or draw
+      {:winner, winner_name} ->
+        {:reply, "#{winner_name} wins!", game_state}
+      :draw ->
+        {:reply, "Game drawn.", game_state}
+      :not_over ->
+        {:reply, "Not over, all players must move.", game_state}
+    end
+  end
+
+  defp check_victory(game_state) when is_map(game_state) do
+    [player_one, player_two] = Map.keys(game_state)
+    %{
+      ^player_one => move_one,
+      ^player_two => move_two
+    } = game_state
+
+    cond do
+      is_nil(move_one) || is_nil(move_two) ->
+        :not_over
+      move_one == move_two ->
+        :draw
+      @defeats[move_one] == move_two ->
+        {:winner, player_one}
+      true ->
+        {:winner, player_two}
+    end
   end
 end
