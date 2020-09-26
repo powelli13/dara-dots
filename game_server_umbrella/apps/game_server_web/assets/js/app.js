c3cbe9c9f570962777b67bcc4e41bbebfbc52718
Isaac Powell <powelli91@outlook.com>
2020-09-26 12:30:22 -0700

    ported over the youtube player from rumbl app and messed around. can't get the player in the iframe to work with LiveView

--- game_server_umbrella/apps/game_server_web/assets/js/app.js
+++ game_server_umbrella/apps/game_server_web/assets/js/app.js
@@ -13,8 +13,14 @@ import "../css/app.scss";
 
 import "phoenix_html";
 
+// JavaScript necessary for the main lobby chat
 import LobbyChat from "./lobby";
+
+// Script for the basic Tic Tac Toe game
 import Game from "./game";
+
+// Video player that is used to play videos on the lip_sync LiveView
+import Player from "./player";
 
 import socket from "./socket";
 
@@ -22,14 +28,22 @@ import socket from "./socket";
 // if it is found on the page.
 LobbyChat.init(socket, document.getElementById("lobby-chat-container"));
 
-// Initialize the Game facilitating object.
-// TODO consider changing the initializing state
+// Initialize the Game facilitating script
 Game.init(socket, "id");
+
+Player.init("video-player-id", "8jTjNMkWOzM", () => console.log("video player ready!"));
+
+let Hooks = {};
+Hooks.VideoPlayer = {
+  mount() {
+    console.log('mounted the player!');
+  }
+};
 
 import LiveSocket from "phoenix_live_view";
 import {Socket} from "phoenix";
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks});
 
 liveSocket.connect();
