bc8985ae941b51aa285d9e9e8cef0cf83fa39ff9
Isaac Powell <powelli91@outlook.com>
2021-01-30 19:23:48 -0800

    practiced with unit testing a GenServer

--- game_server_umbrella/apps/game_server/lib/game_server/rock_paper_scissors.ex
+++ game_server_umbrella/apps/game_server/lib/game_server/rock_paper_scissors.ex
@@ -31,6 +31,14 @@ defmodule GameServer.RockPaperScissors do
     GenServer.cast(game_pid, {:add_player, player_name})
   end
 
+  def get_player_names(game_pid) do
+    GenServer.call(game_pid, :get_player_names)
+  end
+
+  def get_player_moves(game_pid) do
+    GenServer.call(game_pid, :get_player_moves)
+  end
+
   def start_link(game_id) do
     GenServer.start_link(
       __MODULE__,
@@ -40,8 +48,7 @@ defmodule GameServer.RockPaperScissors do
 
   @impl GenServer
   def init(game_id) do
-    # IO.inspect Registry.register(GameServer.Registry, {__MODULE__, game_id}, game_id)
-    # TODO make a struct for this?
+    # TODO make a struct for this
     initial_state = %{
       :game_id => game_id,
       :player_one_name => nil,
@@ -72,24 +79,41 @@ defmodule GameServer.RockPaperScissors do
   end
 
   @impl GenServer
+  def handle_call(:get_player_names, _, game_state) do
+    {
+      :reply,
+      [game_state[:player_one_name], game_state[:player_two_name]],
+      game_state
+    }
+  end
+
+  @impl GenServer
+  def handle_call(:get_player_moves, _, game_state) do
+    {
+      :reply,
+      %{
+        :player_one_move => game_state[:player_one_move],
+        :player_two_move => game_state[:player_two_move]
+      },
+      game_state
+    }
+  end
+
+  @impl GenServer
   def handle_cast({:player_move, player_name, move}, game_state) do
-    IO.puts("Player #{player_name} entered move of #{move}")
     # TODO this does not correctly update, this entire module is pretty gross
-    # should be refactored with tests addeD
+    # should be refactored with tests added
     # should probably handle that using a separate game state struct
     game_state =
       cond do
         game_state[:player_one_name] == player_name ->
-          IO.puts("player one moved")
           Map.put(game_state, :player_one_move, move)
 
         game_state[:player_two_name] == player_name ->
-          IO.puts("player two moved")
           Map.put(game_state, :player_two_move, move)
 
         # Not a valid player name
         true ->
-          IO.puts("no game change")
           game_state
       end
 
@@ -100,7 +124,6 @@ defmodule GameServer.RockPaperScissors do
       {:winner, winner_name} ->
         # Scoreboard.report_win(winner_name)
         broadcast_game_update(game_state[:game_id], {:game_over, winner_name})
-        # TODO report loss and draw
         {:stop, :normal, game_state}
 
       :draw ->
@@ -114,10 +137,6 @@ defmodule GameServer.RockPaperScissors do
   end
 
   defp check_victory(game_state) when is_map(game_state) do
-    IO.puts("game state in check victory")
-    IO.puts(game_state[:player_one_move])
-    IO.puts(game_state[:player_two_move])
-
     %{
       :player_one_name => player_one,
       :player_two_name => player_two,
@@ -141,8 +160,6 @@ defmodule GameServer.RockPaperScissors do
   end
 
   defp broadcast_game_update(game_id, update_term) do
-    IO.puts("RPC broadcasting! Sending game update")
-    IO.puts(update_term)
     PubSub.broadcast(GameServer.PubSub, "game:" <> game_id, update_term)
   end
 end
---
+++ game_server_umbrella/apps/game_server/test/game_server/rock_paper_scissors_test.exs
@@ -0,0 +1,94 @@
+defmodule GameServer.RockPaperScissorsTest do
+  use ExUnit.Case, async: true
+
+  alias GameServer.RockPaperScissors
+  alias Phoenix.PubSub
+
+  # setup?
+  setup do
+    game_id = "test_game_id"
+    {:ok, pid} = GenServer.start_link(GameServer.RockPaperScissors, game_id)
+
+    {:ok, rps_game_pid: pid, game_id: game_id}
+  end
+
+  test "add one player", state do
+    test_name = "Rumplestiltskin"
+
+    RockPaperScissors.add_player(state[:rps_game_pid], test_name)
+    player_names = RockPaperScissors.get_player_names(state[:rps_game_pid])
+
+    assert hd(player_names) == test_name
+  end
+
+  test "add two players", state do
+    test_name_one = "Rumplestiltskin"
+    test_name_two = "Pied Piper"
+
+    RockPaperScissors.add_player(state[:rps_game_pid], test_name_one)
+    RockPaperScissors.add_player(state[:rps_game_pid], test_name_two)
+
+    player_names = RockPaperScissors.get_player_names(state[:rps_game_pid])
+
+    assert hd(player_names) == test_name_one
+    assert hd(tl(player_names)) == test_name_two
+  end
+
+  test "player one makes move", state do
+    test_name = "PlayerOne"
+    test_move = :rock
+
+    RockPaperScissors.add_player(state[:rps_game_pid], test_name)
+
+    RockPaperScissors.enter_move(state[:rps_game_pid], test_name, test_move)
+
+    player_moves = RockPaperScissors.get_player_moves(state[:rps_game_pid])
+
+    assert player_moves[:player_one_move] == test_move
+    assert player_moves[:player_two_move] == nil
+  end
+
+  test "both players make moves", state do
+    # Subscribe to receive endgame state after moves are submitted
+    # RPS server stops after both moves are submitted,
+    # so we subscribe to receive endgame state
+    PubSub.subscribe(GameServer.PubSub, "game:" <> state[:game_id])
+
+    test_name_one = "PlayerOne"
+    test_name_two = "PlayerTwo"
+
+    test_move_one = :rock
+    test_move_two = :paper
+
+    RockPaperScissors.add_player(state[:rps_game_pid], test_name_one)
+    RockPaperScissors.add_player(state[:rps_game_pid], test_name_two)
+
+    RockPaperScissors.enter_move(state[:rps_game_pid], test_name_one, test_move_one)
+    RockPaperScissors.enter_move(state[:rps_game_pid], test_name_two, test_move_two)
+
+    assert_receive {:game_over, winner_name}
+
+    assert winner_name == test_name_two
+  end
+
+  test "both players move game drawn", state do
+    # Subscribe to receive endgame state after moves are submitted
+    # RPS server stops after both moves are submitted,
+    # so we subscribe to receive endgame state
+    PubSub.subscribe(GameServer.PubSub, "game:" <> state[:game_id])
+
+    test_name_one = "PlayerOne"
+    test_name_two = "PlayerTwo"
+
+    test_move_one = :rock
+    test_move_two = :rock
+
+    RockPaperScissors.add_player(state[:rps_game_pid], test_name_one)
+    RockPaperScissors.add_player(state[:rps_game_pid], test_name_two)
+
+    RockPaperScissors.enter_move(state[:rps_game_pid], test_name_one, test_move_one)
+    RockPaperScissors.enter_move(state[:rps_game_pid], test_name_two, test_move_two)
+
+    assert_receive :game_drawn
+  end
+end
