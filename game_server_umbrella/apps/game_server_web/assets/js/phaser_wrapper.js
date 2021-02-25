8157523c17ddd4c312d2e22c21b05506e0047527
Isaac Powell <powelli91@outlook.com>
2021-02-24 17:39:37 -0800

    added game image assets into source control

--- game_server_umbrella/apps/game_server_web/assets/js/phaser_wrapper.js
+++ game_server_umbrella/apps/game_server_web/assets/js/phaser_wrapper.js
@@ -14,22 +14,25 @@ let PhaserWrapper = {
   onReady(socket) {
     console.log('Phaser wrapper is now ready');
 
-    // Only load the Phaser assets on certain pages
+    this.initPhaserGame(socket)
+  },
+
+  initPhaserGame(socket) {
     var config = {
-        type: Phaser.AUTO,
-        width: 800,
-        height: 700,
-        physics: {
-            default: 'arcade',
-            arcade: {
-                gravity: { y: 0 }
-            }
-        },
-        scene: {
-            preload: preload,
-            create: create,
-            update: update
+      type: Phaser.AUTO,
+      width: 450,
+      height: 450,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0 }
         }
+      },
+      scene: {
+        preload: preload,
+        create: create,
+        update: update
+      }
     };
 
     var game = new Phaser.Game(config);
@@ -37,19 +40,22 @@ let PhaserWrapper = {
 
     function preload ()
     {
-        this.load.image('background', 'game_images/background.jpg');
+      this.load.image('background', 'game_images/background.jpg');
+      this.load.image('star_test', 'game_images/star.png');
     }
 
     function create ()
     {
-        // Only load the Phaser assets on certain pages
-        this.add.image(400, 300, 'background');
+      // Only load the Phaser assets on certain pages
+      this.add.image(450, 450, 'background');
+      this.add.image(25, 25, 'star_test');
 
-        this.input.mouse.disableContextMenu();
+      this.input.mouse.disableContextMenu();
     }
 
     function update ()
     {
+      console.log('hi there updating from Phaser');
     }
   }
 };
