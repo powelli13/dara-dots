cfa61d7ae8811c23dcbfe1e8ffc11fb1ae4faff7
Isaac Powell <powelli91@outlook.com>
2021-06-13 00:13:18 -0700

    initial changes to have ball moving based on theta angle and speed

--- game_server_umbrella/apps/game_server/lib/game_server/pong/pong_game_state.ex
+++ game_server_umbrella/apps/game_server/lib/game_server/pong/pong_game_state.ex
@@ -5,6 +5,11 @@ defmodule GameServer.PongGameState do
 
   defstruct ball_x: 0.5,
             ball_y: 0.5,
+            ball_speed: 0.05,
+            # Theta here is in degrees and is converted when used
+            ball_theta: 0,
+            ball_x_step: 0.05,
+            ball_y_step: 0.05,
             bot_paddle_x: 0.05
 
   def move_bottom_paddle(state = %GameServer.PongGameState{}, direction)
@@ -29,9 +34,41 @@ defmodule GameServer.PongGameState do
     %GameServer.PongGameState{state | bot_paddle_x: new_paddle_x}
   end
 
-  def move_ball(state = %GameServer.PongGameState{}) do
-    r = :rand.uniform()
+  defp degrees_to_radians(degrees) do
+    degrees * :math.pi() / 180
+  end
 
-    %GameServer.PongGameState{state | ball_x: r, ball_y: r}
+  def move_ball(state = %GameServer.PongGameState{}) do
+    # check collisions
+    new_theta =
+      cond do
+        state.ball_x <= 0.25 ->
+          0
+
+        state.ball_x >= 0.75 ->
+          180
+
+        true ->
+          state.ball_theta
+      end
+
+    # recalculate x and y step
+    radians = degrees_to_radians(new_theta)
+
+    new_ball_x_step = state.ball_speed * :math.cos(radians)
+    new_ball_y_step = state.ball_speed * :math.sin(radians)
+
+    # move ball
+    new_ball_x = state.ball_x + new_ball_x_step
+    new_ball_y = state.ball_y + new_ball_y_step
+
+    %GameServer.PongGameState{
+      state
+      | ball_x: new_ball_x,
+        ball_y: new_ball_y,
+        ball_x_step: new_ball_x_step,
+        ball_y_step: new_ball_y_step,
+        ball_theta: new_theta
+    }
   end
 end
