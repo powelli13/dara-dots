aa7f126a6ba8a5ac58bda458665194921c4a915c
Isaac Powell <powelli91@outlook.com>
2022-08-11 22:02:59 -0700

    added some new tests around pending actions and board state updates

--- apps/game_server/test/game_server/dara_dots/dara_dots_game_test.exs
+++ apps/game_server/test/game_server/dara_dots/dara_dots_game_test.exs
@@ -1,6 +1,6 @@
 defmodule GameServer.DaraDots.DaraDotsGameTest do
   use ExUnit.Case, async: true
-  alias GameServer.DaraDots.DaraDotsGame
+  alias GameServer.DaraDots.{DaraDotsGame, Coordinate}
 
   # setup do
   # id = "test_id"
@@ -68,6 +68,8 @@ defmodule GameServer.DaraDots.DaraDotsGameTest do
     DaraDotsGame.place_runner(id, "player_one", 1, 3)
 
     after_board = DaraDotsGame.get_full_board(id)
+
+    # TODO lol finish this test
   end
 
   test "multiple pending actions is a valid state" do
@@ -86,5 +88,57 @@ defmodule GameServer.DaraDots.DaraDotsGameTest do
     actual = pending_actions |> Map.keys() |> Enum.count()
 
     assert actual == 2
+  end
+
+  test "place_runner should not update board state" do
+    id = "test_id_6"
+    {:ok, _pid} = DaraDotsGame.start(id)
+
+    # Add both players
+    DaraDotsGame.add_player(id, "player_one")
+    DaraDotsGame.add_player(id, "player_two")
+
+    initial_board = DaraDotsGame.get_full_board(id)
+
+    DaraDotsGame.place_runner(id, "player_one", 1, 3)
+
+    after_board = DaraDotsGame.get_full_board(id)
+
+    initial_count = initial_board.runner_pieces
+      |> Map.keys()
+      |> Enum.count()
+    after_count = after_board.runner_pieces
+      |> Map.keys()
+      |> Enum.count()
+
+    assert initial_count == after_count
+  end
+
+  test "submit_move should not update board state" do
+    id = "test_id_7"
+    {:ok, _pid} = DaraDotsGame.start(id)
+
+    # Add both players
+    DaraDotsGame.add_player(id, "top_player")
+    DaraDotsGame.add_player(id, "bot_player")
+
+    initial_board = DaraDotsGame.get_full_board(id)
+
+    DaraDotsGame.submit_move(id, "top_player", 1, 5)
+
+    after_board = DaraDotsGame.get_full_board(id)
+
+    top_alpha_init_coord = initial_board.top_linker_alpha.coord
+    top_beta_init_coord = initial_board.top_linker_beta.coord
+
+    top_alpha_after_coord = after_board.top_linker_alpha.coord
+    top_beta_after_coord = after_board.top_linker_beta.coord
+
+    # TODO this is passing but I am not sure why
+    IO.inspect top_alpha_init_coord
+    IO.inspect top_alpha_after_coord
+
+    assert Coordinate.equal?(top_alpha_init_coord, top_alpha_after_coord)
+    assert Coordinate.equal?(top_beta_init_coord, top_beta_after_coord)
   end
 end
